3fb2fc1adb4c0d445b8dc4e805e0d3f9bcbb39d8
bonfs <matheusbonfim05@gmail.com>
2020-01-27 16:41:47 -0300

    socket

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // @ts-ignore
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -8,6 +8,40 @@ import TrailTest from './Screens/TrailTest';
 import Result from './Screens/Result';
 
 function Routes () {
+    const [userLogin, setUserLogin] = useState<boolean>(false);
+    const ws = new WebSocket('wss://localhost:6868')
+    useEffect(() => {
+        ws.onopen = () => {
+            console.log('connected')
+        }
+        ws.onclose = () => {
+            console.log('disconnected')
+        }
+        getInfos();
+    }, []);
+
+    useEffect(() => {
+        getInfos();
+    }, [userLogin]);
+
+    function getInfos() {
+        setTimeout(() => {
+            getUserLogin();
+            
+        }, 1000);
+    }
+
+    function getUserLogin(){
+        ws.send(JSON.stringify({"id": 1, "jsonrpc": "2.0", "method": "getUserLogin"}));
+        ws.onmessage = (evt : any) => {
+            if(JSON.parse(evt.data)['result'].length === 0)
+                setUserLogin(false);
+            else
+                setUserLogin(true);
+        }
+    }
+
+
     return(
         <BrowserRouter>
             <Switch>
